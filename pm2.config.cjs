ac604d96af4011a3a46e87e8f9292c593c155f2a
Aseem Shrey <aseemshrey@gmail.com>
2025-11-01 19:54:39 -0400

    fix: configure pm2 with swc binary path

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,3 +1,10 @@
+const path = require('path');
+
+const swcBinaryPath = path.join(
+  __dirname,
+  'node_modules/.bun/@swc+core-darwin-arm64@1.13.21/node_modules/@swc/core-darwin-arm64/swc.darwin-arm64.node',
+);
+
 module.exports = {
   apps: [
     {
@@ -26,6 +33,7 @@ module.exports = {
       env_file: __dirname + '/worker/.env',
       env: {
         TEMPORAL_TASK_QUEUE: 'shipsec-default',
+        SWC_BINARY_PATH: swcBinaryPath,
       },
     },
     {
@@ -36,6 +44,7 @@ module.exports = {
       env_file: __dirname + '/worker/.env',
       env: {
         TEMPORAL_TASK_QUEUE: 'test-worker-integration',
+        SWC_BINARY_PATH: swcBinaryPath,
       },
     },
   ],
